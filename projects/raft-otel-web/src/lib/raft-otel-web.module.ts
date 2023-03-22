a7004f932e1d390d666d7ad70bdc3acf92cada48
rocket-x <zcjb.rjdj@gmail.com>
2023-03-22 23:00:22 +0800

    remove files

--- projects/raft-otel-web/src/lib/raft-otel-web.module.ts
+++ projects/raft-otel-web/src/lib/raft-otel-web.module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
-import { RaftOtelWebComponent } from './raft-otel-web.component';
-
-
-
 @NgModule({
-  declarations: [
-    RaftOtelWebComponent
-  ],
-  imports: [
-  ],
-  exports: [
-    RaftOtelWebComponent
-  ]
+  declarations: [],
+  imports: [],
+  exports: [],
 })
-export class RaftOtelWebModule { }
+export class RaftOtelWebModule {}
--- projects/raft-otel-web/src/public-api.ts
+++ projects/raft-otel-web/src/public-api.ts
@@ -1,7 +1,4 @@
 /*
  * Public API Surface of raft-otel-web
  */
-
-export * from './lib/raft-otel-web.service';
-export * from './lib/raft-otel-web.component';
 export * from './lib/raft-otel-web.module';
